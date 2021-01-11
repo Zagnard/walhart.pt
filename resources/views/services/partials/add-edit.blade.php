9b58e6b1dcb4ab16825a0f275e9654de51ffd75a
godzmatos <2190588@my.ipleiria.pt>
2021-01-11 18:54:38 +0000

    2021-01-11-18-54

--- resources/views/services/partials/add-edit.blade.php
+++ resources/views/services/partials/add-edit.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
     <label for="inputName">Name</label>
-    <input type="text" class="form-control" name="name" id="inputName" value="" />
+    <input type="text" class="form-control" name="name" id="inputName" value="{{old('nome',$services->nome)}}" />
 </div>
 <div class="form-group">
     <label for="inputDescription">Description</label>
-    <textarea class="form-control" name="description" id="inputDescription"></textarea>
+    <textarea class="form-control" name="description" id="inputDescription">{{old('descricao',$services->descricao)}}</textarea>
 </div>
