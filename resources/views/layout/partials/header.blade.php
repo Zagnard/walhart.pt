a0b3ef962ec4295c5c48b70e0f008052aad4cf5f
Alex Torres <alex-98-dylan@hotmail.com>
2021-02-05 22:12:36 +0000

    header and footer reso

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,59 +1,31 @@
-<div class="menu">
-<ul>
-    <li>
-        <a href="">
-            Home
-        </a>
-    </li>
-    <li>
-        <a href="{{ route('wl.services') }}">
-            Serviços
-        </a>
-    </li>
-    <li>
-        <a href="">
-            Estúdio
-        </a>
-    </li>
-    <li>
-        <a href="">
-            Portfólio
-        </a>
-    </li>
-    <li>
-        <a href="">
-            F.A.Q.
-        </a>
-		</li>
-		<li>
-			<a href="">
-				Listar as faq
-			</a>
-		</li>
-		<li>
-			<a href="">
-				Criar nova faq
-			</a>
-		</li>
-    <li>
-        <a href="">
-            Registo
-        </a>
-    </li>
-    <li>
-        <a href="">
-            Login
-        </a>
-    </li>
-    <li>
-        <a href="">
-            Perfil de utilizador
-        </a>
-    </li>
-    <li>
-        <a href="">
-            Edição de dados pessoais
-        </a>
-    </li>
-</ul>
-</div>
+<header>
+  <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img width="80px" src="http://localhost/portfolio/public/imagens/logo_walhart_pequeno.png">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item ">
+            <a class="nav-link" aria-current="page" href="">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Serviços</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Estúdio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('portfolio')  }}">Portfólio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">F.a.Q.</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
