d4736c0c6cc11830af9e365ffe86c9ad2d918b98
Alex Torres <alex-98-dylan@hotmail.com>
2021-02-07 19:46:46 +0000

    ze

---
+++ app/Http/Requests/UpdateportfoliosRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateportfoliosRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    { 
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "nome_beat" => 'required|min:3|max:20|' .$this->portfolios->id . '|regex:/^[A-ZÀ-úa-z\s]+$/',
+            "descricao" => 'required',
+            "tipo" => 'required'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'nome_beat.regex' => '2-20'
+        ];
+    }
+}
