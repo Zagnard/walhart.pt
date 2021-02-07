2565e9ab9ae29b6f22eca57d30bdacf372ca95e2
Alex Torres <alex-98-dylan@hotmail.com>
2021-02-07 22:12:23 +0000

    change

--- resources/views/portfolio/partials/add-edit.blade.php
+++ resources/views/portfolio/partials/add-edit.blade.php
@@ -1,12 +1,12 @@
 <div class="form-group">
     <label for="inputnome_beat">Nome</label>
-    <textarea class="form-control" name="nome_beat" id="inputnome_beat">{{old('nome_beat',$portfolio->nome_beat)}}</textarea>
+    <textarea class="form-control" name="nome_beat" id="inputnome_beat">{{old('nome_beat',$portfolios->nome_beat)}}</textarea>
 </div>
 <div class="form-group">
     <label for="inputdescricao">Descrição</label>
-    <textarea class="form-control" name="descricao" id="inputdescricao">{{old('descricao',$portfolio->descricao)}}</textarea>
+    <textarea class="form-control" name="descricao" id="inputdescricao">{{old('descricao',$portfolios->descricao)}}</textarea>
 </div>
 <div class="form-group">
     <label for="inputtipo">Tipo</label>
-    <textarea class="form-control" name="tipo" id="inputtipo">{{old('tipo',$portfolio->tipo)}}</textarea>
+    <textarea class="form-control" name="tipo" id="inputtipo">{{old('tipo',$portfolios->tipo)}}</textarea>
 </div>
